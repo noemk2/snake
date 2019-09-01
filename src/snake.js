3bab1e9505df16c24bc7e1d3524a09f4d44515c7
Noe Huaccharaque <noemk2@gmail.com>
2019-09-01 02:37:50 -0500

    start function draw() for rendering grafic

---
+++ src/snake.js
@@ -0,0 +1,15 @@
+// 2. haciendo el renderizado 👿 👿
+
+//variables 🐽 
+const SPEED = 80,
+    SIZE = 10;
+
+//function draw() 🌈
+const draw = () => {
+    //extrayendo a canvas del html
+    let canvas = document.getElementById("canvas"),
+        ctx = canvas.getContext("2d");
+    //limpiando campo, estamos haciendo un cuadro de limpieza
+    ctx.clearRect(0, 0, camvas.width, canvas.height);
+}
+
