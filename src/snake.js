8a3ffc7610d367429b9b55b258876d8840a85040
Noe Huaccharaque <noemk2@gmail.com>
2019-09-01 16:47:34 -0500

    Heredar una clase para crear objetos (Factory)

--- src/snake.js
+++ src/snake.js
@@ -4,18 +4,41 @@
 const SPEED = 80,
     SIZE = 10;
 
+//heredando clase para crear => objetos para mi juego  
+class Objetos {
+    constructor() {
+        this.size = SIZE;
+    }
+    // method choque
+    // el choque lo evaluaremos la diferencia entre posiciones X and Y
+    choque(obj) {
+        let difx = Math.abs(this.x - obj.x);
+        let dify = Math.abs(this.y - obj.y);
+        //evaluando las diferencias
+        (difx >= 0 || difx < this.size || dify >= 0 || dify < this.size) ? true : false;
+    }
+};
+//heredando de Objetos
+class Cola extends Objetos{
+    constructor(x,y) {
+        //almacenando el valor de x en x ... same y
+        this.x = x ;
+        this.y = y ;
+    }
+}
+
 //function draw() 🌈
 const draw = () => {
     //extrayendo a canvas del html
     let canvas = document.getElementById("canvas"),
         ctx = canvas.getContext("2d");
     //limpiando campo, estamos haciendo un cuadro de limpieza
-    ctx.clearRect(0, 0, camvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     //aqui abajo va todo el dibujo
 }
 // function animate ... main()✊✊ 
 const main = () => {
     draw();
-    
+
 }
 setInterval("main()", SPEED)
