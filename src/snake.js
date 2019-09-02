b984be341fd027ffff2fc045ae6f60398fab0b68
Noe Huaccharaque <noemk2@gmail.com>
2019-09-02 07:43:19 -0500

    Crear clase comida de class Objeto y crear un punto aleatorio y modificar el tamaño del canvas html

--- src/snake.js
+++ src/snake.js
@@ -3,7 +3,7 @@
 //variables 🐽 
 const SPEED = 80,
     SIZE = 10;
-
+//Clase padre
 //heredando clase para crear => objetos para mi juego  
 class Objetos {
     constructor() {
@@ -19,8 +19,7 @@ class Objetos {
     }
 };
 
-
-//heredando de Objetos
+//heredando de Objetos... 🐥🐥🐥
 //creando la clase que va ser el recipiente de nuestro objeto
 class Cola extends Objetos {
     constructor(x, y) {
@@ -36,18 +35,45 @@ class Cola extends Objetos {
         ctx.fillStyle = "#03a9f4"
         //creamos el cuadro que formara nuestra snake
         ctx.fillRect(this.x, this.y, this.size, this.size)
+        //p = posicion
+        //f = forma en pixeles
+        //ctx.fillRect(p en el eje x, p en el eje y, f mas + es mas larga, f mas + es mas ancha )
     }
     // 6_ method setter 💁💁💁 porque no accedemos atraves de el objeto ya creado (new Cola)   
+    //creando especie de atributo instanciado
     setxy(x, y) {
         this.x = x;
         this.y = y;
     }
 }
+//8_ crear comida
+class Comida extends Objetos{
+    // el punto va tener un posicion random
+    constructor() {
+        super();
+        this.x = this.generete();
+        this.y = this.generete();
+    }
+    generete(){
+        //me genera numeros entre 0-580 de diez en diez
+        let num = (Math.floor(Math.random()*59))*10;
+        return num;
+    }
+    colocar(){
+        // se va llamar con cada colicion
+        this.x = generete();
+        this.y = generete();
+    }
+    draw(ctx){
+        ctx.fillStyle = "#f25056"
+        ctx.fillRect(this.x, this.y, this.size, this.size)
+    }
+}
 
-//instanciado de Cola class
+//instanciado de Cola class 🌠🌠🌠
 // Objetos del juego
 let cabeza = new Cola(20, 20);
-
+let comida = new Comida();
 // 6_ Variables de ilucion de movimiento 🙉🙉🙉
 // estos boleanos nos permitiran habilitar o bloquear los ejes ¬ de modo que no podamos movernos en el mismo eje
 let ejex = true,
@@ -58,7 +84,7 @@ let xdir = 0,
 
 // 6_ function movimiento
 const movimiento = () => {
-    // obtenemos la posicion y le sumamos el valor de xy direction
+    // obtenemos la posicion (cabeza.x) y le sumamos el valor de xy direction
     let nx = cabeza.x + xdir,
         ny = cabeza.y + ydir;
     //accedemos al objeto creado
@@ -68,6 +94,7 @@ const movimiento = () => {
 const control = (e) =>{
     // esta propiedad (keyCode) nos da el codigo de la tecla
     let cod = e.keyCode
+    //revisar doc de switch
     if (ejex) {
         if (cod == 38) {
             ydir = -SIZE;
@@ -108,6 +135,8 @@ const draw = () => {
     //aqui abajo va todo el dibujo
     //dibujando mi cabeza
     cabeza.draw(ctx);
+    comida.draw(ctx);
+
 };
 // function animate ... main()✊✊ 
 const main = () => {
