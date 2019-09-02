2b142e3f785e83db91b65098aa7ab24b2b2417f8
Noe Huaccharaque <noemk2@gmail.com>
2019-09-01 19:38:57 -0500

    Creando draw() de Cola class e instanciandolo y dibujando la cabeza

--- src/snake.js
+++ src/snake.js
@@ -15,17 +15,33 @@ class Objetos {
         let difx = Math.abs(this.x - obj.x);
         let dify = Math.abs(this.y - obj.y);
         //evaluando las diferencias
-        (difx >= 0 || difx < this.size || dify >= 0 || dify < this.size) ? true : false;
+        (difx >= 0 || difx < this.size || dify >= 0 || dify < this.size) ? true: false;
     }
 };
+
+
 //heredando de Objetos
-class Cola extends Objetos{
-    constructor(x,y) {
+//creando la clase que va ser el recipiente de nuestro objeto
+class Cola extends Objetos {
+    constructor(x, y) {
+        //llamando al contructor padre
+        super();
         //almacenando el valor de x en x ... same y
-        this.x = x ;
-        this.y = y ;
+        this.x = x;
+        this.y = y;
+    }
+    // 5 (branch). create method draw() de forma independiente 🌋🌋🌋
+    draw(ctx) {
+        //darle Color
+        ctx.fillStyle = "#03a9f4"
+        //creamos el cuadro que formara nuestra snake
+        ctx.fillRect(this.x, this.y, this.size, this.size)
     }
 }
+
+//instanciado de Cola class
+// Objetos del juego
+let cabeza = new Cola(20,20);
 
 //function draw() 🌈
 const draw = () => {
@@ -35,6 +51,8 @@ const draw = () => {
     //limpiando campo, estamos haciendo un cuadro de limpieza
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //aqui abajo va todo el dibujo
+        //dibujando mi cabeza
+    cabeza.draw(ctx);
 }
 // function animate ... main()✊✊ 
 const main = () => {
